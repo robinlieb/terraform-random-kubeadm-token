2d73e01a58fdd297929c0d83f0c7f878a2bc79d3
Robin Lieb <robin.j.lieb@gmail.com>
2022-12-18 10:18:30 +0100

    feat: added description to output

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,4 @@
 output "token" {
-  value = format("%s.%s", random_string.token_id.result, random_string.token_secret.result)
+  value       = format("%s.%s", random_string.token_id.result, random_string.token_secret.result)
+  description = "The token which can be used for kubeadm. This will match the regular expression [a-z0-9]{6}.[a-z0-9]{16}"
 }
