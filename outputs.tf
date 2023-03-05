463035def1f103d977777c2f7e03c58a590f29d5
Robin Lieb <robin.j.lieb@gmail.com>
2023-03-05 11:42:13 +0100

    feat: set token output to sensitive

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,5 @@
 output "token" {
   value       = format("%s.%s", random_string.token_id.result, random_string.token_secret.result)
   description = "The token which can be used for kubeadm. This will match the regular expression [a-z0-9]{6}.[a-z0-9]{16}"
+  sensitive = true
 }
